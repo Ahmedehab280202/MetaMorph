53b99300ef09042e21f859010ec4cc8abf8f539b
Ahmedehab280202 <ahmedehab280202@gmail.com>
2024-02-20 19:42:30 +0200

    json perparation

--- Figma Plugin (Design)/code.ts
+++ Figma Plugin (Design)/code.ts
@@ -1,34 +1,50 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+"use strict";
+const selectLayer = figma.currentPage.selection[0];
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+const jsonProcess = (node: any) => ({
+  id: node.id,
+  name: node.name,
+  layout: {
+    x: node.x,
+    y: node.y,
+    layoutMode: node.layoutMode,
+    primaryAxisAlignItems: node.primaryAxisAlignItems,
+    counterAxisAlignItems: node.counterAxisAlignItems,
+    itemSpacing: node.itemSpacing,
+  },
+  box: {
+    width: node.width,
+    height: node.height,
+    parentLayoutMode: node.parentLayoutMode,
+    parentNodeType: node.parentNodeType,
+    layoutMode: node.layoutMode,
+    layoutGrow: node.layoutGrow,
+    layoutAlign: node.layoutAlign,
+    primaryAxisSizingMode: node.primaryAxisSizingMode,
+    counterAxisSizingMode: node.counterAxisSizingMode,
+    paddingLeft: node.paddingLeft,
+    paddingTop: node.paddingTop,
+    paddingRight: node.paddingRight,
+    paddingBottom: node.paddingBottom,
+  },
+  design: {
+    fills: node.fills,
+    strokes: node.strokes,
+    effects: node.effects,
+    strokeWeight: node.strokeWeight,
+    topLeftRadius: node.topLeftRadius,
+    topRightRadius: node.topRightRadius,
+    bottomRightRadius: node.bottomRightRadius,
+    bottomLeftRadius: node.bottomLeftRadius,
+  },
+  children: (
+    node.children ?
+      node.children.map((child: any) => {
+        return jsonProcess(child);
+      }) : null
+  )
+})
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+console.log('selectLayer:');
+console.log(selectLayer);
+console.log(JSON.stringify(jsonProcess(selectLayer)));
