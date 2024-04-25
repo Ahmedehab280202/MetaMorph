d5abbe916da18f2ca38e1b4cb231a00177ab793a
70dakhaled <mahmoud2008975@miuegypt.edu.eg>
2024-04-25 15:25:40 +0000

    Update index.ts
    hotfix

--- Java_Generator/src/index.ts
+++ Java_Generator/src/index.ts
@@ -5,24 +5,31 @@ import ControllerFile from "./models/ControllerFile/ControllerFile";
 import ServiceFile from "./models/ServiceFile/ServiceFile";
 import RepositoryFile from "./models/RepositoryFile/RepositoryFile";
 const app = express();
-const port = 3000;
+const port = 3006;
 // Middleware to parse JSON requests
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
+app.post("/springboot", (req: Request, res: Response) => {
   const requestbody: any = req.body;
-  const class_node: ClassNode = requestbody.class_nodes[0];
-  const model_file: ModelFile = new ModelFile(class_node);
-  const controller_file: ControllerFile = new ControllerFile(class_node);
-  const service_file: ServiceFile = new ServiceFile(class_node);
-  const repository_file: RepositoryFile = new RepositoryFile(class_node);
+  //const class_node: ClassNode = requestbody.class_nodes[0];
+ const result= requestbody.class_nodes.map((class_node:ClassNode) => {
+    
+    return {
+      "name":class_node.name,
+      "model_file":new ModelFile(class_node).toString(),
+      "controller_file":new ControllerFile(class_node).toString(),
+      "service_file":new ServiceFile(class_node).toString(),
+      "repository_file":new RepositoryFile(class_node).toString(),
+    } 
+  });
+  
+  // //const classeslist:<ClassNode>;
+  // const model_file: ModelFile = new ModelFile(class_node);
+  // const controller_file: ControllerFile = new ControllerFile(class_node);
+  // const service_file: ServiceFile = new ServiceFile(class_node);
+  // const repository_file: RepositoryFile = new RepositoryFile(class_node);
 
   //output 
-  res.send({
-    "controller": controller_file.toString(),
-    "model": model_file.toString(),
-    "service": service_file.toString(),
-    "repository": repository_file.toString()
-  });
+  res.send(result);
 
 
 });
