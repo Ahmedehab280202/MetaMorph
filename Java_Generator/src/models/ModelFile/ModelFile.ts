e8e9cd469031150a4726673f08439e1bf1963e89
70dakhaled <mahmoud2008975@miuegypt.edu.eg>
2024-04-25 15:25:03 +0000

    Update ModelFile.ts
    relationship attempt

--- Java_Generator/src/models/ModelFile/ModelFile.ts
+++ Java_Generator/src/models/ModelFile/ModelFile.ts
@@ -4,12 +4,26 @@ import ModelProp from "./ModelProp";
 export default class ModelFile {
   private class_name: String;
   private prop_nodes: PropNode[];
-
+  private generalization: Object;
   constructor(class_node: ClassNode) {
     this.class_name = class_node.name;
     this.prop_nodes = class_node.prop_nodes;
+    this.generalization = (
+      class_node.relashionships.find(rel => "Inheritance" in rel)|| 
+      class_node.relashionships.find(rel => "Implements" in rel) ||
+      {}
+    );
   }
+
   toString() {
+    const gen_code: String= (
+      "Inheritance"in this.generalization
+      ? "extends " +this.generalization["Inheritance"]
+      :"Implements" in this.generalization
+      ? "implements " +this.generalization["Implements"]
+      :"" 
+    ) ;
+
    return( `
         package com.meta.model;
         import jakarta.persistence.*;
@@ -19,7 +33,7 @@ export default class ModelFile {
         @Data
         @AllArgsConstructor
         @NoArgsConstructor
-        public class ${this.class_name} {
+        public class ${this.class_name} ${gen_code}{
             @Id
             @GeneratedValue(strategy = GenerationType.IDENTITY)
             private int id;
