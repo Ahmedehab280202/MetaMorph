e85eb63eaa47c33fac1ed9e0278281112e9b4c02
6802ali <ali2006367@miuegypt.edu.eg>
2024-05-04 03:56:14 +0300

    implemented the frontend code inspection !

--- metamorph-ui/src/pages/ViewCodePage.jsx
+++ metamorph-ui/src/pages/ViewCodePage.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ViewCodeNavBar from "../components/viewcode/ViewCodeNavBar";
-import ViewCodeComponent from "../components/viewcode/ViewCodeComponent";
 import ViewCodeSideUtilityBar from "../components/viewcode/ViewCodeSideUtilityBar";
-import CodeEditor from "../components/viewcode/CodeEditor";
 import "../CSS/code_view/viewcodepagewrapper.css";
 
 const ViewCodePage = () => {
   return (
     <div className="view-code-page-wrapper">
       <ViewCodeNavBar />
-      {/* <div className="code-view-body"> */}
-        <ViewCodeSideUtilityBar />
-        {/* <ViewCodeComponent /> */}
-        {/* <div className="code-inspection"> */}
-          {/* <CodeEditor /> */}
-          {/* <ViewCodeComponent /> */}
-        {/* </div> */}
-      {/* </div> */}
+      <ViewCodeSideUtilityBar />
     </div>
   );
 };
