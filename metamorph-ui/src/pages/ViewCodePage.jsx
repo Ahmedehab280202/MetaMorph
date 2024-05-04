d9733a9782f42d27cef2e35e5639850d05b8d3d8
6802ali <ali2006367@miuegypt.edu.eg>
2024-05-04 04:01:12 +0300

    Revert "implemented the frontend code inspection !"
    This reverts commit e85eb63eaa47c33fac1ed9e0278281112e9b4c02.

--- metamorph-ui/src/pages/ViewCodePage.jsx
+++ metamorph-ui/src/pages/ViewCodePage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ViewCodeNavBar from "../components/viewcode/ViewCodeNavBar";
+import ViewCodeComponent from "../components/viewcode/ViewCodeComponent";
 import ViewCodeSideUtilityBar from "../components/viewcode/ViewCodeSideUtilityBar";
+import CodeEditor from "../components/viewcode/CodeEditor";
 import "../CSS/code_view/viewcodepagewrapper.css";
 
 const ViewCodePage = () => {
   return (
     <div className="view-code-page-wrapper">
       <ViewCodeNavBar />
-      <ViewCodeSideUtilityBar />
+      {/* <div className="code-view-body"> */}
+        <ViewCodeSideUtilityBar />
+        {/* <ViewCodeComponent /> */}
+        {/* <div className="code-inspection"> */}
+          {/* <CodeEditor /> */}
+          {/* <ViewCodeComponent /> */}
+        {/* </div> */}
+      {/* </div> */}
     </div>
   );
 };
--- metamorph-ui/src/services/FrontEndCodeService.js
+++ metamorph-ui/src/services/FrontEndCodeService.js
@@ -2,26 +2,40 @@ import axios from "axios";
 
 import { FrontEndModel } from "../models/FrontEndDataModel";
 
-const baseUrl = "http://localhost:8080/metadata/project";
+const baseUrl = "http://localhost:8080/metadata/project/metamorphtest";
 
-export const getFrontEndData = async (projectName) => {
+export const getFrontEndData = async (FrontEndModel) => {
+
     try {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        throw new Error("No token found in localStorage");
-      }
-      const headers = {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      };
-      const response = await axios.get(`${baseUrl}/${projectName}`, { headers });
-      console.log("response:", response);
-      return response.data;
+        const token = localStorage.getItem("token");
+        const data = {
+            html_code:FrontEndModel.html_code,
+            css_code: FrontEndModel.css_code,
+        };
+
+        console.log("test 1 getFrontEndData service test test test test !!!!!")
+
+        const headers = {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        };
+
+        console.log("test 2 getFrontEndData service test test test test !!!!!")
+
+        console.log("fetched data:", data);
+        
+        const response = await axios.get(baseUrl, data, { headers });
+
+        console.log("test 3 getFrontEndData service test test test test !!!!!")
+
+        console.log("Response:", response.data);
+
+        // Return the response data
+        return response.data;
     } catch (error) {
-      console.log("Error from metadata service:", error);
-      if (error.response && error.response.data) {
+        console.log("Error from metadata service try catch:", error);
+
+        // Return the error response data
         return error.response.data;
-      }
-      return { data: error.message };
     }
-  };
+};
